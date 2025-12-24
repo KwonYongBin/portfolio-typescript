6bfc6873d10b3b475cce38762e80ce03f64b3a39
KwonYongBin <dydqls6789@naver.com>
2025-12-24 10:58:33 +0900

    폴도 생성및 구조화 작업

---
+++ protfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import TypeScriptstudy from "./page/TypeScriptstudy";
+import './assets/style/style.scss';
+
+function App() {
+
+  return (
+    <>
+      <h1>type strat</h1>
+      <TypeScriptstudy />
+    </>
+  )
+}
+
+export default App
---
+++ protfolio/src/page/TypeScriptstudy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TypeScriptstudy = () => {
+  return (
+    <div>연습</div>
+  )
+}
+
+export default TypeScriptstudy;
